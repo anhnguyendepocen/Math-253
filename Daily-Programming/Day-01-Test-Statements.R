089e75a167be1b10c9db034f531b1a945c5cf359
dtkaplan <dtkaplan@gmail.com>
2015-10-06 20:51:14 -0500

    Wrote test statements for Day 01

---
+++ Daily-Programming/Day-01-Test-Statements.R
@@ -0,0 +1,16 @@
+require(scoreActivity)
+res <- score_set(
+  check(TRUE, "package:mosaic" %in% search()),
+  check(TRUE, is.character(task2)),
+  check(TRUE, grepl("^Today is",task2) && grepl("201.$", task2)),
+  check(TRUE, all(task3a %in% names(mosaicData::Galton))),
+  check(nrow(mosaicData::Galton), task3b),
+  check(about(mean(mosaicData::Galton$height)), task3c),
+  check(TRUE, all(dim(task4) == c(2,3))),
+  check(TRUE, max(task5x) <= 1 && min(task5x) >= 0),
+  check(TRUE, max(task5y) <= 1 && min(task5y) >= 0),
+  check(about(pi, pm=.5), task5pi),
+  leave_out_names = c("mosaicData", "height")
+)
+res$score
+cat(res$comments)
---
+++ Daily-Programming/Day-10-Test-Statements.R
@@ -0,0 +1,17 @@
+library(scoreActivity)
+res <- score_set(
+  check_exists(Default),
+  check_exists(logistic),
+  check(about(0.5), logistic(0)),
+  check(about(0.8807, pm=0.01), logistic(2)),
+  check(about(88724.25), 
+        linear_combine(data = Default, coef = c(intercept=1, income=2))[1]),
+  check(TRUE, gives_error(linear_combine(data=Default, coef=c(intercept=1, whoopee = 2)))),
+  check(TRUE, is.function(LL_logistic)),
+  check(about(-2425.7), LL_logistic(data=Default, coefs = c(intercept = 1, income=-.0001), outcome = Default$default == "Yes")),
+  check_exists(best_coefs),
+  check(TRUE, "par" %in% names(best_coefs)),
+  leave_out_names = "default"
+)
+res$score
+cat(res$comments)
