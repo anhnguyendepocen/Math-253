ffebdcac66dd4a8e83a6377ef11600141ee8e83a
dtkaplan <dtkaplan@gmail.com>
2015-10-15 09:01:45 -0500

    Added score253() function to scoreActivity package

---
+++ Daily-Programming/scoreActivity/R/score-253.R
@@ -0,0 +1,18 @@
+#' Score a daily activity in Math 253
+#'
+#' This function is specifically designed for Fall 2015
+#'
+#' @param day.  The day of the activity.  This identifies which test
+#' statements are to be used.
+#'
+#' @export
+score253 <- function(day) {
+  if (missing(day)) stop("Must specify the day as an argument, e.g. day = 14")
+  day <- as.numeric(day)
+  if (is.na(day)) stop("day = ", day, "doesn't look like a number!")
+  day <- if (day < 10 ) paste0("0",day) else as.character(day)
+  git_location <- "https://raw.githubusercontent.com/dtkaplan/Math-253/master/"
+  test_file <- paste0("Daily-Programming/Day-",day,"-Test-Statements.R")
+  cat("--------\nRunning test statements for day ", day, "\n\n")
+  source(paste0(git_location, test_file))
+  }
