85bbfa4ad2b65c2fb950d48d963875f2459612eb
dtkaplan <dtkaplan@gmail.com>
2015-10-22 13:09:30 -0500

    fix to test statements for day 4

--- Daily-Programming/Day-04-Test-Statements.R
+++ Daily-Programming/Day-04-Test-Statements.R
@@ -5,8 +5,8 @@ Test_Statements <- score_set(
   check(TRUE, diff(range(x1)) == diff(range(y1))),
   check(5, length(x2)),
   check_exists(x3),
-  check(about(50), mean(x3)),
-  check(about(60), mean(y3)),
+  check(about(50, pm=1), mean(x3)),
+  check(about(60, pm=1), mean(y3)),
   check(about(40), diff(range(x3))),
   check_exists(x4),
   check_exists(y4)
