167c595a29c5d2ac94b10ca015c323ae1d43e4cb
dtkaplan <dtkaplan@gmail.com>
2015-10-22 17:23:15 -0500

    Day 13 programming task and test statements

---
+++ Daily-Programming/Day-13-Test-Statements.R
@@ -0,0 +1,15 @@
+library(scoreActivity)
+library(mosaicData)
+Test_Statements <- score_set(
+  check_exists(k_fold1),
+  check_exists(k_fold),
+  check(TRUE, "method" %in% names(formals(k_fold1))),
+  check(TRUE, "data" %in% names(formals(k_fold1))),
+  check(about(8, pm=3), k_fold1(), pts=2),
+  check(TRUE, "For_Testing" %in% all.names(body(k_fold1))),
+  check(TRUE, "predfun" %in% all.names(body(k_fold))),
+  check(TRUE, "method" %in% all.names(body(k_fold))),
+  check(about(24, pm=4), k_fold(wage ~ age + sex, data=mosaicData::CPS85)),
+  leave_out_names = c("wage", "age", "sex", "mosaicData")
+)
+print_test_results(Test_Statements)
