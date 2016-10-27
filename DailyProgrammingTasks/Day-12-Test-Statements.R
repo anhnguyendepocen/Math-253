4e45dc679ca7fec57142c672ee1924cc056d9a10
dtkaplan <dtkaplan@gmail.com>
2016-10-27 10:10:46 -0500

    Updating test statements since edited activity 12

--- DailyProgrammingTasks/Day-12-Test-Statements.R
+++ DailyProgrammingTasks/Day-12-Test-Statements.R
@@ -5,7 +5,7 @@ Test_Statements <- score_set(
   check_exists(Blue),
   check(TRUE, all(names(Red) %in% c("x", "y", "class"))),
   check(TRUE, all(abs(apply(red, FUN=mean, MARGIN=2)-red_mean) < .5)),
-  check(TRUE, all(table(Sim_one$class) == c(n_cases, n_cases))),
+  check(TRUE, all(table(Same_sigma$class) == c(n_cases, n_cases))),
   check_exists(mod_LDA_one),
   check_exists(mod_QDA_two),
   check_exists(test_QDA_one),
